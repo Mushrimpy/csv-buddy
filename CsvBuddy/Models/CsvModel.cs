f50e8883fa2d7fa958b547ac3fc4ee73b52065dd
Moncef Slimani <moncef.slimani.ms@gmail.com>
2025-03-18 15:21:59 +0000

    Create csv model

---
+++ CsvBuddy/Models/CsvModel.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace CsvBuddy.Models
+{
+    public class CsvField(string value, bool isQuoted = false)
+    {
+        private string Value { get; set; } = value;
+        public bool IsQuoted { get; set; } = isQuoted;
+    }
+    public abstract class CsvRecord
+    {
+        private List<CsvField> Fields { get; } = new List<CsvField>();
+        public void AddField(CsvField field) => Fields.Add(field);
+        public void AddField(string value, bool isQuoted = false) => Fields.Add(new CsvField(value, isQuoted));
+        public int FieldCount => Fields.Count;
+        public CsvField this[int index] => Fields[index];
+    }
+
+    public class CsvFile(string? filename = null)
+    {
+        private List<CsvRecord> Records { get; } = new List<CsvRecord>();
+        public string? Filename { get; set; } = filename;
+        public void AddRecord(CsvRecord record) => Records.Add(record);
+        public int RecordCount => Records.Count;
+        public CsvRecord this[int index] => Records[index];
+    }
+}
